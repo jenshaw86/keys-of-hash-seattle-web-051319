968b47b8fee35068e6829c4c6fdee6913c99ec7e
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-12 18:18:10 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,8 +1,11 @@
 require "pry"
 class Hash
   def keys_of(arguments)
+    keys = []
     self.each do |animal, origin|
-      binding.pry
+      if origin == arguments
+        keys << arguments
+      end
     end
   end
 end
