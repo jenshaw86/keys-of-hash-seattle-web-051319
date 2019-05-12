af60df47df87b153397187786e13801fb406f365
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-12 18:49:53 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,8 +3,10 @@ class Hash
   def keys_of(*arguments)
     keys = []
     self.each do |animal, origin|
-      if origin == arguments
-        keys << animal
+      arguments.each do |argument|
+        if origin == arguments
+          keys << animal
+        end
       end
     end
     keys
